768f18139d92696c27a1b3676a1ba4d9d488fdbe
Hupan <hupan008@qq.com>
2019-01-09 17:03:04 +0800

    D01

---
+++ D01/HelloWorld.java
@@ -0,0 +1,15 @@
+/*Java介绍
+关键字
+编译解释
+虚拟机运行环境开发工具
+变量
+命名规范
+注释
+editplus编辑器
+*/
+public class HelloWorld{
+    //程序入口
+    public static void main(String[] args){
+        System.out.println("Hello World!againnnnnnnnn");
+    }
+}
