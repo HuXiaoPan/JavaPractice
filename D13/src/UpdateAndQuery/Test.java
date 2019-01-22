863847156f2d8228b986bdd38c894e78b1b83a4b
HuXiaoPan <hupan008@qq.com>
2019-01-22 22:54:09 +0800

    JDBC工具类

--- D13/src/UpdateAndQuery/Test.java
+++ D13/src/UpdateAndQuery/Test.java
@@ -1,29 +1,33 @@
-package JDBCTest;
+package UpdateAndQuery;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.mysql.jdbc.Driver;
 
-public class JDBCTest {
+public class Test {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		//DriverManager.registerDriver(new Driver());
 		Class.forName("com.mysql.jdbc.Driver");
-		
+
 		String url = "jdbc:mysql://192.168.31.32:3306/mybase?useSSL=false";
 		String name = "root";
 		String pwd = "123456";
-		
 		Connection conn = DriverManager.getConnection(url, name, pwd);
-		System.out.println(conn);
-		
 		Statement stat = conn.createStatement();
-		stat.executeUpdate("insert into sort(sname,sprice,sdesc) values('汽车用品',50000,'疯狂涨价')");
-		
+
+		ResultSet rs = stat.executeQuery("select * from sort;");
+
+		while (rs.next()) {
+			System.out.println(rs.getInt("sid") + "    " + rs.getString("sname") + "    " + rs.getDouble("sprice")
+					+ "    " + rs.getString("sdesc"));
+		}
+
+		rs.close();
 		stat.close();
 		conn.close();
 	}
